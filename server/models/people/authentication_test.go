3e9b344f896e892f4333087ab3f6633fb0706e37
Louis Duchemin <ls.duchemin@gmail.com>
2024-03-19 10:00:57 +0100

    Fix user authentication test

--- server/models/people/authentication_test.go
+++ server/models/people/authentication_test.go
@@ -3,9 +3,11 @@ package people_test
 import (
 	"darco/proto/db"
 	users "darco/proto/models/people"
-	"errors"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestActivationAuthentication(t *testing.T) {
@@ -13,9 +15,7 @@ func TestActivationAuthentication(t *testing.T) {
 	defer teardown()
 	db := db.Client()
 
-	if user.EmailConfirmed {
-		t.Fatalf("New user email must not be already confirmed when first created.")
-	}
+	assert.Falsef(t, user.EmailConfirmed, "New user email must not be already confirmed when first created.")
 
 	var credentials = users.UserCredentials{
 		Identifier: "mock.user",
@@ -23,27 +23,20 @@ func TestActivationAuthentication(t *testing.T) {
 	}
 
 	_, auth_err := credentials.Authenticate(db)
-	if auth_err == nil {
-		t.Fatalf("Inactive user should not be able to authenticate.")
-	} else {
-		var inactive = new(users.LoginFailedError)
-		if !errors.As(auth_err, &inactive) || inactive.Reason != users.AccountInactive {
-			t.Fatalf("Authentication denied to inactive user but has unexpected error reason, got %v", inactive)
-		}
-
-	}
+	require.Errorf(t, auth_err, "Inactive user should not be able to authenticate.")
+	assert.Equal(t, auth_err.Reason, users.AccountInactive)
 
 	// if err := user.SetActive(db, true); err != nil {
 	// 	t.Fatalf("Failed to activate user account: %v", err)
 	// }
 
+	require.NoError(t, user.SetEmailConfirmed(db, true))
+	person, err := FakePersonInput(t).Create(db)
+	require.NoError(t, err)
+	require.NoError(t, user.SetIdentity(db, &person))
 	auth_user, err := credentials.Authenticate(db)
-	if err != nil {
-		t.Fatalf("Failed authentication with valid credentials: %v", err)
-	}
-	if auth_user.ID != user.ID {
-		t.Fatalf("Authentication succeeded, but does not return the expected user.")
-	}
+	assert.Nil(t, err)
+	assert.Equal(t, user, auth_user)
 
 	var invalidCredentials = []users.UserCredentials{
 		{"invalid.identifier", "whateverpassword"},
@@ -55,9 +48,8 @@ func TestActivationAuthentication(t *testing.T) {
 			fmt.Sprintf("Invalid creds %s:%s", creds.Identifier, creds.Password),
 			func(t *testing.T) {
 				u, err := creds.Authenticate(db)
-				if err == nil || u != nil {
-					t.Fatalf("Authentication succeeded with invalid credentials")
-				}
+				require.Error(t, err)
+				assert.Nil(t, u)
 			})
 	}
 }
