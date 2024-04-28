c30f8351e9614aa38df59f7b67e09fc9ca287007
Louis Duchemin <ls.duchemin@gmail.com>
2024-04-28 18:34:23 +0200

    Create password field component with togglable visibility

---
+++ client/src/components/toolkit/ui/PasswordField.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-text-field
+    v-model="model"
+    v-bind="$attrs"
+    :type="show ? 'text' : 'password'"
+    :append-inner-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+    @click:append-inner="show = !show"
+  />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const model = defineModel<string>()
+const show = ref(false)
+</script>
+
+<style scoped></style>
