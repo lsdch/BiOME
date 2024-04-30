1be91e66c60c85e6a4cc4281caac6b6d4cb74abd
Louis Duchemin <ls.duchemin@gmail.com>
2024-04-30 11:30:33 +0200

    Improved app settings navigation

--- client/src/views/settings/AdminSettings.vue
+++ client/src/views/settings/AdminSettings.vue
@@ -2,36 +2,40 @@
   <div :class="`d-flex ${xs ? 'flex-column' : 'flex-row'}`">
     <v-tabs v-model="tab" color="primary" slider-color="primary" :direction="direction">
       <v-tab
-        value="Instance"
+        value="instance"
         text="Instance"
         prepend-icon="mdi-application-settings-outline"
         tile
         :rounded="false"
+        href="#instance"
       />
       <v-tab
-        value="Security"
+        value="security"
         text="Security"
         :tile="true"
         prepend-icon="mdi-security"
         :rounded="false"
+        href="#security"
       />
-      <v-tab value="Email" text="E-mail" prepend-icon="mdi-email" tile :rounded="false" />
+      <v-tab value="email" text="E-mail" prepend-icon="mdi-email" tile :rounded="false" />
     </v-tabs>
     <v-container>
       <v-row>
         <v-col>
           <Suspense>
-            <v-window v-model="tab" class="pa-3" :direction="direction">
-              <v-window-item value="Instance">
-                <InstanceSettings />
-              </v-window-item>
-              <v-window-item value="Security">
-                <SecuritySettings />
-              </v-window-item>
-              <v-window-item value="Email">
-                <EmailSettings />
-              </v-window-item>
-            </v-window>
+            <template #default>
+              <v-window v-model="tab" class="pa-3" :direction="direction">
+                <v-window-item value="instance">
+                  <InstanceSettings />
+                </v-window-item>
+                <v-window-item value="security">
+                  <SecuritySettings />
+                </v-window-item>
+                <v-window-item value="email">
+                  <EmailSettings />
+                </v-window-item>
+              </v-window>
+            </template>
             <template #fallback>
               <v-skeleton-loader type="article, article, article"> </v-skeleton-loader>
             </template>
@@ -49,16 +53,28 @@ import SecuritySettings from '@/components/settings/SecuritySettings.vue'
 import EmailSettings from '@/components/settings/EmailSettings.vue'
 import { useDisplay } from 'vuetify'
 import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { watch } from 'vue'
 
 const { xs } = useDisplay()
 
-type SettingsTab = 'Instance' | 'Security' | 'Email'
+type SettingsTab = 'instance' | 'security' | 'email'
 
 const direction = computed(() => {
   return xs.value ? 'horizontal' : 'vertical'
 })
 
-const tab = ref<SettingsTab>('Instance')
+const route = useRoute()
+function hashToTab(hash: string) {
+  return hash.startsWith('#') ? (hash.slice(1) as SettingsTab) : 'instance'
+}
+const tab = ref(hashToTab(route.hash))
+watch(
+  () => route.hash,
+  (hash: string) => {
+    tab.value = hashToTab(hash)
+  }
+)
 </script>
 
 <style scoped></style>
