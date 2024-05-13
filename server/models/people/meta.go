62fc4f1b0ce382356edc10020f0bc4d2fc5e0bd2
Louis Duchemin <ls.duchemin@gmail.com>
2024-05-13 14:12:00 +0200

    Fix optional values in Meta

--- server/models/people/meta.go
+++ server/models/people/meta.go
@@ -16,10 +16,10 @@ type UserShortIdentity struct {
 type Meta struct {
 	ID            edgedb.UUID             `edgedb:"id" json:"-" swaggerignore:"true"`
 	Created       time.Time               `edgedb:"created" json:"created" binding:"required"`
-	Modified      edgedb.OptionalDateTime `edgedb:"modified" json:"modified"`
+	Modified      edgedb.OptionalDateTime `edgedb:"modified" json:"modified,omitempty"`
 	LastUpdated   time.Time               `edgedb:"lastUpdated" json:"last_updated"`
-	CreatedByUser OptionalUser            `json:"created_by_user" edgedb:"created_by_user"`
-	UpdatedByUser OptionalUser            `json:"updated_by_user" edgedb:"modified_by_user"`
-	CreatedBy     UserShortIdentity       `json:"created_by" edgedb:"created_by"`
-	UpdatedBy     UserShortIdentity       `json:"updated_by" edgedb:"updated_by"`
+	CreatedByUser OptionalUser            `json:"created_by_user,omitempty" edgedb:"created_by_user"`
+	UpdatedByUser OptionalUser            `json:"updated_by_user,omitempty" edgedb:"modified_by_user"`
+	CreatedBy     UserShortIdentity       `json:"created_by,omitempty" edgedb:"created_by"`
+	UpdatedBy     UserShortIdentity       `json:"updated_by,omitempty" edgedb:"updated_by"`
 } // @name Meta
