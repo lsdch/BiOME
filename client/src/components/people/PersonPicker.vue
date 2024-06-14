0c2ab75b6a78c6e169dbeb63dfe67b0b0f08b32f
Louis Duchemin <ls.duchemin@gmail.com>
2024-06-14 11:58:27 +0200

    PersonPicker component

---
+++ client/src/components/people/PersonPicker.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-autocomplete
+    v-model="model"
+    :multiple="multiple"
+    :label="label"
+    :items="_items"
+    item-title="full_name"
+    clearable
+    chips
+    closable-chips
+    auto-select-first
+    clear-on-select
+    counter
+  >
+  </v-autocomplete>
+</template>
+
+<script setup lang="ts">
+import { PeopleService, Person, PersonUser } from '@/api'
+
+const props = defineProps<{
+  multiple?: boolean
+  label: string
+  items?: Person[]
+}>()
+
+const _items = props.items ?? (await PeopleService.listPersons())
+
+const model = defineModel<PersonUser[] | PersonUser>()
+</script>
+
+<style lang="scss" scoped></style>
