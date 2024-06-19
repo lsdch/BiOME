ca0ad322bf2b4bcb7d40f69bb56a33533138fe32
Louis Duchemin <ls.duchemin@gmail.com>
2024-06-19 11:49:35 +0200

    fix: openapi path

--- client/openapi-ts.config.ts
+++ client/openapi-ts.config.ts
@@ -2,7 +2,7 @@ import { UserConfig, defineConfig } from "@hey-api/openapi-ts"
 
 const config: UserConfig = defineConfig({
   client: "@hey-api/client-fetch",
-  input: "../server/docs/openapi.json",
+  input: "./openapi.json",
   output: {
     path: "src/api",
     format: "prettier",
