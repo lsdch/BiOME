a93b0de42b7d4e3096479f908ca162db58ff2c37
Louis Duchemin <ls.duchemin@gmail.com>
2024-07-30 13:02:07 +0200

    implement validation errors utility type

--- server/models/validations/errors.go
+++ server/models/validations/errors.go
@@ -4,8 +4,29 @@ import (
 	"fmt"
 	"reflect"
 
+	"github.com/danielgtaylor/huma/v2"
 	"github.com/go-playground/validator/v10"
 )
+
+// A slice of validation errors which allows bulk manipulation such as updating error locations
+type ValidationErrors []*huma.ErrorDetail
+
+func (e ValidationErrors) Errors() []error {
+	var errors = make([]error, len(e))
+	for _, err := range e {
+		errors = append(errors, err)
+	}
+	return errors
+}
+
+// Prepends an error location string to all errors in the slice.
+// Don't forget '.' separators when needed.
+func (e ValidationErrors) WithLocation(location string) []error {
+	for _, err := range []*huma.ErrorDetail(e) {
+		err.Location = location + err.Location
+	}
+	return e.Errors()
+}
 
 // Structured validation error built from go-playground/validator to be consumed by API clients
 // @description A validation error to be fixed in the input
