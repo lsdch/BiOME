c1be022b3d91598fd41a72714969b1097f1007a7
Louis Duchemin <ls.duchemin@gmail.com>
2024-09-09 12:42:39 +0200

    several features and fixes on SitesMap component

--- client/src/components/maps/SitesMap.vue
+++ client/src/components/maps/SitesMap.vue
@@ -1,91 +1,108 @@
 <template>
-  <l-map
-    ref="map"
-    v-model:zoom="zoom"
-    :center="[0, 0]"
-    :use-global-leaflet="true"
-    v-model:bounds="mapBounds"
-    :max-bounds="latLngBounds(latLng(90, -360), latLng(-90, 360))"
-    :max-bounds-viscosity="1.0"
-    :min-zoom="2"
-    :max-zoom="18"
-    @ready="onReady"
-    :options="{
-      gestureHandling: true,
-      worldCopyJump: true,
-      wheelPxPerZoomLevel: 100,
-      zoomSnap: 0.5
-    }"
-  >
-    <LControlScale position="bottomleft" metric :imperial="false" />
-    <LControl position="topright" class="ma-0 d-flex justify-end">
-      <v-btn
-        v-if="closable"
-        title="Close"
-        color="white"
-        :rounded="false"
-        icon="mdi-close"
-        :width="35"
-        :height="35"
-        density="compact"
-        @click="emit('close')"
-      />
-    </LControl>
-
-    <LControl position="topleft">
-      <div class="leaflet-bar">
+  <div ref="map" class="fill-height" v-element-visibility="onVisible">
+    <l-map
+      v-model:zoom="zoom"
+      v-bind="$attrs"
+      :center="[0, 0]"
+      :use-global-leaflet="true"
+      v-model:bounds="mapBounds"
+      :max-bounds="latLngBounds(latLng(90, -360), latLng(-90, 360))"
+      :max-bounds-viscosity="1.0"
+      :min-zoom="2"
+      :max-zoom="18"
+      @ready="onReady"
+      :options="{
+        gestureHandling: true,
+        worldCopyJump: true,
+        wheelPxPerZoomLevel: 100,
+        zoomSnap: 0.5
+      }"
+    >
+      <LControlScale position="bottomright" metric :imperial="false" />
+      <LControl position="topright" class="ma-0 d-flex justify-end">
         <v-btn
-          v-if="items"
-          title="Fit view"
+          v-if="closable"
+          title="Close"
           color="white"
+          class="bg-white"
           :rounded="false"
-          icon="mdi-fit-to-screen"
-          :width="30"
+          icon="mdi-close"
+          :width="35"
+          :height="35"
           density="compact"
-          @click="fitBounds(items)"
+          @click="emit('close')"
         />
-      </div>
-    </LControl>
+      </LControl>
 
-    <LControlLayers hide-single-base />
+      <LControl position="topleft">
+        <div class="leaflet-bar d-flex flex-column">
+          <v-btn
+            v-if="items"
+            title="Fit view"
+            class="bg-white"
+            color="white"
+            :rounded="false"
+            icon="mdi-fit-to-screen"
+            :width="30"
+            density="compact"
+            @click="fitBounds(items)"
+          />
+          <v-btn
+            title="Toggle fullscreen"
+            color="white"
+            class="bg-white"
+            :rounded="false"
+            :icon="isFullscreen ? 'mdi-fullscreen-exit' : 'mdi-fullscreen'"
+            :width="30"
+            density="compact"
+            @click="toggleFullscreen"
+          />
+        </div>
+      </LControl>
+      <LControl position="topleft">
+        <div class="leaflet-bar"></div>
+      </LControl>
 
-    <l-tile-layer
-      :subdomains="['server', 'services']"
-      url="https://{s}.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-      attributionUrl="https://static.arcgis.com/attribution/World_Imagery"
-      attribution="Powered by <a href='https://www.esri.com/'>Esri</a> &mdash; Source: Esri, Maxar, Earthstar Geographics, and the GIS User Community"
-      layer-type="base"
-      name="Base layer"
-    />
-    <l-tile-layer
-      :subdomains="['server', 'services']"
-      url="https://{s}.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}"
-      layer-type="overlay"
-      name="Regions"
-      :opacity="0.75"
-      :visible="false"
-    />
-    <LMarkerClusterGroup v-if="clustered" remove-outside-visible-bounds show-coverage-on-hover>
+      <LControlLayers hide-single-base />
+
+      <l-tile-layer
+        :subdomains="['server', 'services']"
+        url="https://{s}.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+        attributionUrl="https://static.arcgis.com/attribution/World_Imagery"
+        attribution="Powered by <a href='https://www.esri.com/'>Esri</a> &mdash; Source: Esri, Maxar, Earthstar Geographics, and the GIS User Community"
+        layer-type="base"
+        name="Base layer"
+      />
+      <l-tile-layer
+        :subdomains="['server', 'services']"
+        url="https://{s}.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}"
+        layer-type="overlay"
+        name="Regions"
+        :opacity="0.75"
+        :visible="regions"
+      />
+      <LMarkerClusterGroup v-if="clustered" remove-outside-visible-bounds show-coverage-on-hover>
+        <LCircleMarker
+          v-for="(item, key) in items"
+          :key
+          :lat-lng="[item.coordinates.latitude, item.coordinates.longitude]"
+          v-bind="marker"
+        >
+          <slot name="marker" :item />
+        </LCircleMarker>
+      </LMarkerClusterGroup>
       <LCircleMarker
+        v-else
         v-for="(item, key) in items"
         :key
-        :lat-lng="[item.coordinates.latitude, item.coordinates.longitude]"
+        :latLng="[item.coordinates.latitude, item.coordinates.longitude]"
         v-bind="marker"
       >
         <slot name="marker" :item />
       </LCircleMarker>
-    </LMarkerClusterGroup>
-    <LCircleMarker
-      v-else
-      v-for="(item, key) in items"
-      :key
-      :latLng="[item.coordinates.latitude, item.coordinates.longitude]"
-      v-bind="marker"
-    >
-      <slot name="marker" :item />
-    </LCircleMarker>
-    <slot :map></slot>
-  </l-map>
+      <slot :map></slot>
+    </l-map>
+  </div>
 </template>
 
 <script setup lang="ts" generic="SiteItem extends Geocoordinates">
@@ -95,9 +112,9 @@ import {
   LControlLayers,
   LControlScale,
   LMap,
-  LMarker,
   LTileLayer
 } from '@vue-leaflet/vue-leaflet'
+import { onKeyStroke, useFullscreen, useThrottleFn } from '@vueuse/core'
 import L, {
   CircleMarkerOptions,
   latLng,
@@ -106,20 +123,19 @@ import L, {
   LatLngLiteral,
   type Map
 } from 'leaflet'
-import 'leaflet.fullscreen'
-import 'leaflet.fullscreen/Control.FullScreen.css'
 import 'leaflet/dist/leaflet.css'
 import { ref, watch } from 'vue'
-import { Geocoordinates } from '.'
-import { onKeyStroke, useFullscreen } from '@vueuse/core'
 import { LMarkerClusterGroup } from 'vue-leaflet-markercluster'
+import { Geocoordinates } from '.'
+
+import { vElementVisibility } from '@vueuse/components'
 
 const zoom = ref(1)
 const map = ref<HTMLElement>()
 
-const { isFullscreen, enter, exit } = useFullscreen(map)
-onKeyStroke('f', enter)
+const { isFullscreen, enter, exit, toggle } = useFullscreen(map, {})
 onKeyStroke('Escape', exit)
+const toggleFullscreen = useThrottleFn(toggle)
 
 const emit = defineEmits<{
   close: []
@@ -133,9 +149,12 @@ const props = withDefaults(
     autoFit?: boolean
     closable?: boolean
     clustered?: boolean
+    regions?: boolean
+    fitPad?: number
   }>(),
   {
-    bounds: () => [latLng(90, -360), latLng(-90, 360)]
+    bounds: () => [latLng(90, -360), latLng(-90, 360)],
+    fitPad: 0
   }
 )
 
@@ -153,8 +172,12 @@ watch(
 
 function onReady(mapInstance: Map) {
   console.log('Map ready')
-  L.control.fullscreen().addTo(mapInstance)
-  fitBounds()
+  // nextTick(fitBounds)
+  setTimeout(fitBounds, 200)
+}
+
+function onVisible(visible: boolean) {
+  if (visible) fitBounds()
 }
 
 function fitBounds(items: SiteItem[] = props.items ?? []) {
@@ -183,6 +206,10 @@ function fitBounds(items: SiteItem[] = props.items ?? []) {
   )
 
   if (minMaxCoords) {
+    minMaxCoords.sw.lat -= props.fitPad
+    minMaxCoords.sw.lng -= props.fitPad
+    minMaxCoords.ne.lat += props.fitPad
+    minMaxCoords.ne.lng += props.fitPad
     mapBounds.value = latLngBounds(minMaxCoords.sw, minMaxCoords.ne).pad(0.1)
   }
 }
