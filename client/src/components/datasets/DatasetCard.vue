8621285456ec41d884e4da136e7345de745b4e02
Louis Duchemin <ls.duchemin@gmail.com>
2024-09-09 12:41:20 +0200

    feat: improved dataset cards presentation

--- client/src/components/datasets/DatasetCard.vue
+++ client/src/components/datasets/DatasetCard.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-card>
+  <v-card v-bind="$attrs">
     <template #title>
       {{ dataset.label }}
     </template>
     <v-divider></v-divider>
     <template #text>
       {{ dataset.description }}
-      <v-chip prepend-icon="mdi-map-marker">{{ dataset.sites.length }} sites</v-chip>
+      <v-chip prepend-icon="mdi-map-marker">{{ dataset.sites?.length || 'No' }} sites</v-chip>
     </template>
     <template #actions>
       <ItemDateChip :date="dataset.meta.last_updated" icon="mdi-update"></ItemDateChip>
