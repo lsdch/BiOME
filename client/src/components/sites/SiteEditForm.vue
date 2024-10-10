7227816f31640ee99139fe31880244d4bab11616
Louis Duchemin <ls.duchemin@gmail.com>
2024-10-10 11:16:52 +0200

    add site item view

---
+++ client/src/components/sites/SiteEditForm.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-form>
+    <v-row>
+      <v-col>
+        <v-text-field v-model="model.name" label="Name" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-text-field v-model="model.code" label="Code" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-textarea v-model="model.description" label="Description" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="4">
+        <NumberInput v-model="model.coordinates.latitude" label="Latitude" float />
+      </v-col>
+      <v-col cols="12" sm="4">
+        <NumberInput v-model="model.coordinates.longitude" label="Longitude" float />
+      </v-col>
+      <v-col cols="12" sm="4">
+        <CoordPrecisionPicker v-model="model.coordinates.precision" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="8">
+        <v-text-field label="Locality" v-model="model.locality" />
+      </v-col>
+      <v-col cols="12" sm="4">
+        <CountryPicker v-model="model.country" />
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
+
+<script setup lang="ts">
+import { Site, SiteUpdate } from '@/api'
+import CountryPicker from '../toolkit/forms/CountryPicker.vue'
+import CoordPrecisionPicker from './CoordPrecisionPicker.vue'
+import NumberInput from '../toolkit/ui/NumberInput.vue'
+import { ref, watch } from 'vue'
+
+const props = defineProps<{ site: Site }>()
+
+const model = ref(props.site)
+
+watch(
+  () => props.site,
+  (s) => (model.value = s)
+)
+
+function updateInput(site: Site): SiteUpdate {
+  const { name, code, coordinates, description, altitude, locality } = site
+  return {
+    name,
+    code,
+    coordinates,
+    description,
+    altitude,
+    locality,
+    country_code: site.country.code
+  }
+}
+</script>
+
+<style scoped></style>
--- client/src/views/location/SiteItemView.vue
+++ client/src/views/location/SiteItemView.vue
@@ -1,53 +1,66 @@
 <template>
   <div class="bg-surface fill-height">
-    <TableToolbar :title="site.name" icon="mdi-map-marker"></TableToolbar>
     <v-container class="align-start" fluid>
-      <v-row>
-        <v-col cols="12" md="6">
-          <v-list>
-            <v-list-item title="Code" :subtitle="site.code"></v-list-item>
-            <v-list-item title="Description" :subtitle="site.description"></v-list-item>
-          </v-list>
-        </v-col>
-        <v-col cols="12" md="6" style="height: 50vh">
-          <v-card class="fill-height d-flex flex-column">
-            <template #prepend>
-              <v-icon icon="mdi-crosshairs-gps"></v-icon>
-            </template>
-            <template #subtitle>
-              <div class="d-flex justify-space-between flex-wrap">
-                <div>
-                  Coordinates: {{ site.coordinates.latitude }}, {{ site.coordinates.longitude }}
-                  <v-chip :text="site.coordinates.precision"></v-chip>
+      <SiteEditForm v-if="editing" :site />
+      <div v-else>
+        <v-row>
+          <v-col class="text-h5 d-flex align-center justify-space-between">
+            {{ site.name }}
+            <v-btn color="primary" icon="mdi-pencil" variant="plain" @click="toggleEdit(true)" />
+          </v-col>
+        </v-row>
+        <v-divider class="my-3" />
+
+        <v-row>
+          <v-col cols="12" md="6">
+            <v-list>
+              <v-list-item title="Code" :subtitle="site.code"></v-list-item>
+              <v-list-item title="Description" :subtitle="site.description"></v-list-item>
+            </v-list>
+          </v-col>
+          <v-col cols="12" md="6" style="height: 50vh; min-height: 500px">
+            <v-card class="fill-height d-flex flex-column">
+              <template #prepend>
+                <v-icon icon="mdi-crosshairs-gps"></v-icon>
+              </template>
+              <template #subtitle>
+                <div class="d-flex justify-space-between flex-wrap">
+                  <div>
+                    Coordinates: {{ site.coordinates.latitude }},
+                    {{ site.coordinates.longitude }}
+                    <v-chip :text="site.coordinates.precision"></v-chip>
+                  </div>
+                  <div>
+                    {{ [site.locality, site.country.name].filter((e) => e).join(', ') }}
+                    <v-chip :text="site.country.code"></v-chip>
+                  </div>
                 </div>
-                <div>
-                  {{ [site.locality, site.country.name].filter((e) => e).join(', ') }}
-                  <v-chip :text="site.country.code"></v-chip>
-                </div>
-              </div>
-            </template>
-            <SitesMap :items="[site]" regions :fitPad="0.3"></SitesMap>
-            <template #actions>
-              <!-- :href="`https://www.google.com/maps/place/${site.coordinates.latitude}+${site.coordinates.longitude}/@${site.coordinates.latitude},${site.coordinates.longitude},10z`" -->
-              <v-btn
-                prepend-icon="mdi-google-maps"
-                color="primary"
-                :href="`https://earth.google.com/web/search/${site.coordinates.latitude},${site.coordinates.longitude},10z`"
-                text="See in Google Maps"
-              />
-            </template>
-          </v-card>
-        </v-col>
-      </v-row>
+              </template>
+              <SitesMap :items="[site]" regions :fitPad="0.3"></SitesMap>
+              <template #actions>
+                <!-- :href="`https://www.google.com/maps/place/${site.coordinates.latitude}+${site.coordinates.longitude}/@${site.coordinates.latitude},${site.coordinates.longitude},10z`" -->
+                <v-btn
+                  prepend-icon="mdi-google-maps"
+                  color="primary"
+                  :href="`https://earth.google.com/web/search/${site.coordinates.latitude},${site.coordinates.longitude},10z`"
+                  text="See in Google Maps"
+                />
+              </template>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
     </v-container>
   </div>
 </template>
 
 <script setup lang="ts">
-import { LocationService } from '@/api'
+import { LocationService, SiteUpdate } from '@/api'
 import { handleErrors } from '@/api/responses'
 import SitesMap from '@/components/maps/SitesMap.vue'
-import TableToolbar from '@/components/toolkit/tables/TableToolbar.vue'
+import SiteEditForm from '@/components/sites/SiteEditForm.vue'
+import { useToggle } from '@vueuse/core'
+import { useRouteQuery } from '@vueuse/router'
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 
@@ -61,6 +74,22 @@ const site = ref(
     })
   )
 )
+
+const [editing, toggleEdit] = useToggle()
+
+async function submit() {
+  const { name, code, description, coordinates, locality, altitude } = site.value
+  const updatedSite: SiteUpdate = {
+    name,
+    code,
+    coordinates,
+    locality,
+    altitude,
+    description,
+    country_code: site.value.country.code
+  }
+  return await LocationService.updateSite({ body: updatedSite, path: { code } })
+}
 </script>
 
 <style scoped></style>
