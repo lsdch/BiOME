6f367a47ee8c9866f54497a18406bb3de30604ae
Louis Duchemin <ls.duchemin@gmail.com>
2024-10-10 13:42:06 +0200

    refactor route registration in main package

--- server/main.go
+++ server/main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"darco/proto/controllers/institution"
-	country "darco/proto/controllers/location"
-	"darco/proto/controllers/person"
-	"darco/proto/controllers/settings"
-	"darco/proto/controllers/sites"
-	"darco/proto/controllers/taxonomy"
-	accounts "darco/proto/controllers/users"
 	"darco/proto/db"
 	mw "darco/proto/middlewares"
 	"darco/proto/models/location"
+	"darco/proto/models/settings"
 	"darco/proto/router"
 	"darco/proto/services/email"
+	"fmt"
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/gin-gonic/gin"
@@ -22,9 +17,16 @@ import (
 
 //go:generate go run generators/generate_enums.go
 
-func humaConfig(basePath string) huma.Config {
-	var cfg = huma.DefaultConfig("Proto API", "1.0")
-	cfg.Info.Description = "DarCo Prototype API"
+func apiConfig(basePath string) huma.Config {
+	instance := settings.Instance()
+	title := fmt.Sprintf("%s API", instance.Name)
+	description, ok := instance.Description.Get()
+	if !ok {
+		description = instance.Name
+	}
+
+	var cfg = huma.DefaultConfig(title, "1.0")
+	cfg.Info.Description = fmt.Sprintf("%s API", description)
 	cfg.Components.SecuritySchemes = map[string]*huma.SecurityScheme{
 		"bearer": {
 			Type:         "http",
@@ -53,16 +55,9 @@ func humaConfig(basePath string) huma.Config {
 }
 
 func setupRoutes(r *gin.Engine, basePath string) *gin.RouterGroup {
-	router := router.New(r, basePath, humaConfig(basePath))
-	accounts.RegisterRoutes(router)
-	institution.RegisterRoutes(router)
-	person.RegisterRoutes(router)
-	country.RegisterRoutes(router)
-	taxonomy.RegisterRoutes(router)
-	taxonomy.RegisterImportRoutes(router)
-	settings.RegisterRoutes(router)
-	sites.RegisterRoutes(router)
-	sites.RegisterDatasetRoutes(router)
+	apiConfig := apiConfig(basePath)
+	router := router.New(r, basePath, apiConfig)
+	registerRoutes(router)
 	if err := router.WriteSpecJSON("../client/openapi.json"); err != nil {
 		panic(err)
 	}
@@ -80,29 +75,12 @@ func setupRouter() *gin.Engine {
 	return r
 }
 
-// func setupValidators() {
-// 	if engine, ok := binding.Validator.Engine().(*validator.Validate); ok {
-// 		validations.RegisterValidators(engine)
-// 		// Use json names
-// 		engine.RegisterTagNameFunc(func(fld reflect.StructField) string {
-// 			name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
-// 			// skip if tag key says it should be ignored
-// 			if name == "-" {
-// 				return ""
-// 			}
-// 			return name
-// 		})
-// 	}
-// }
-
 func main() {
 
 	gin.ForceConsoleColor()
 	if gin.Mode() == gin.DebugMode {
 		log.SetLevel(log.DebugLevel)
 	}
-
-	// setupValidators()
 
 	if err := email.LoadTemplates("templates/**"); err != nil {
 		log.Fatalf("Failed to load email templates: %v", err)
---
+++ server/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"darco/proto/controllers/institution"
+	"darco/proto/controllers/location"
+	"darco/proto/controllers/person"
+	"darco/proto/controllers/settings"
+	"darco/proto/controllers/sites"
+	"darco/proto/controllers/taxonomy"
+	users "darco/proto/controllers/users"
+	"darco/proto/router"
+)
+
+func registerRoutes(router router.Router) {
+	users.RegisterRoutes(router)
+	institution.RegisterRoutes(router)
+	person.RegisterRoutes(router)
+	location.RegisterRoutes(router)
+	taxonomy.RegisterRoutes(router)
+	taxonomy.RegisterImportRoutes(router)
+	settings.RegisterRoutes(router)
+	sites.RegisterRoutes(router)
+	sites.RegisterDatasetRoutes(router)
+}
