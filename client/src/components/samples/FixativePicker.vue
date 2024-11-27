467fbc64176a629180158163b22b73a743baa35e
Louis Duchemin <ls.duchemin@gmail.com>
2024-11-27 11:35:13 +0100

    Implement FixativePicker

---
+++ client/src/components/samples/FixativePicker.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-select
+    v-model="model"
+    :items
+    :loading
+    item-title="label"
+    item-subtitle="description"
+    prepend-inner-icon="mdi-snowflake"
+    v-bind="$attrs"
+  ></v-select>
+</template>
+
+<script setup lang="ts">
+import { Fixative, SamplingService } from '@/api'
+import { useFetchItems } from '@/composables/fetch_items'
+import { ref } from 'vue'
+
+const model = ref<Fixative[]>([])
+const { items, loading } = useFetchItems(SamplingService.listFixatives)
+</script>
+
+<style scoped lang="scss"></style>
