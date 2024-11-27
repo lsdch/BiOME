8f139afd8831a40d2046c07ca9ef17c133fba7ad
Louis Duchemin <ls.duchemin@gmail.com>
2024-11-27 11:35:28 +0100

    Implement SamplingMethodPicker

---
+++ client/src/components/events/SamplingMethodPicker.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-autocomplete
+    :items
+    :loading
+    item-title="label"
+    item-subtitle="description"
+    v-bind="$attrs"
+    prepend-inner-icon="mdi-hook"
+  >
+  </v-autocomplete>
+</template>
+
+<script setup lang="ts">
+import { SamplingService } from '@/api'
+import { useFetchItems } from '@/composables/fetch_items'
+
+const { items, loading } = useFetchItems(SamplingService.listSamplingMethods)
+</script>
+
+<style scoped lang="scss"></style>
