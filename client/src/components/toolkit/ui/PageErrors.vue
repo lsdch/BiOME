06f343f363c17f2d3733a6e695e1fa0d3973bc1b
Louis Duchemin <ls.duchemin@gmail.com>
2024-11-29 11:01:57 +0100

    Implement PageErrors component to display API error responses on item pages

---
+++ client/src/components/toolkit/ui/PageErrors.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-alert color="error" v-bind="errorsMap[error.status ?? 500]" />
+  </v-container>
+</template>
+
+<script setup lang="ts" generic="T">
+import { ErrorModel } from '@/api'
+
+defineProps<{ error: ErrorModel }>()
+
+type ErrorInfos = {
+  icon: string
+  text: string
+}
+const errorsMap: Record<number, ErrorInfos> = {
+  404: { icon: 'mdi-close-circle', text: 'Item does not exist.' },
+  401: { icon: 'mdi-lock', text: 'Access denied.' },
+  500: { icon: 'mdi-alert-box', text: 'Unexpected server error.' }
+}
+</script>
+
+<style scoped lang="scss"></style>
