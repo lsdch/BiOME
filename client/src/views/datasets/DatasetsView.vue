bc2cd86bcf8b56c6547b57b7ec7aec3641cbbdb6
Louis Duchemin <ls.duchemin@gmail.com>
2024-12-17 17:33:07 +0100

    use CRUDTable in DatasetsView

--- client/src/views/datasets/DatasetsView.vue
+++ client/src/views/datasets/DatasetsView.vue
@@ -1,44 +1,32 @@
 <template>
-  <TableToolbar title="Datasets" icon="mdi-folder-table">
-    <template #search>
-      <v-inline-search-bar label="Search" v-model="search" />
+  <CRUDTable
+    class="fill-height"
+    :headers
+    :fetch-items="DatasetsService.listDatasets"
+    entity-name="Dataset"
+    :toolbar="{ title: 'Datasets', icon: 'mdi-folder-table' }"
+  >
+    <template #item.label="{ item }">
+      <RouterLink :to="{ name: 'dataset-item', params: { slug: item.slug } }" :text="item.label" />
     </template>
-    <template #append>
-      <v-btn text="Import" :to="{ name: 'import-dataset' }" />
-    </template>
-  </TableToolbar>
-  <v-progress-linear v-if="loading" indeterminate />
-  <v-container fluid>
-    <v-row>
-      <v-col v-if="!filteredDatasets?.length" class="text-center">No datasets to display</v-col>
-      <v-col v-for="(dataset, key) in filteredDatasets" :key sm="6">
-        <DatasetCard :dataset :to="{ name: 'dataset-item', params: { slug: dataset.slug } }" />
-      </v-col>
-    </v-row>
-  </v-container>
+  </CRUDTable>
 </template>
 
 <script setup lang="ts">
 import { DatasetsService } from '@/api'
-import DatasetCard from '@/components/datasets/DatasetCard.vue'
-import TableToolbar from '@/components/toolkit/tables/TableToolbar.vue'
-import { useToggle } from '@vueuse/core'
-import { computed, ref } from 'vue'
+import CRUDTable from '@/components/toolkit/tables/CRUDTable.vue'
 
-const [loading, toggleLoading] = useToggle(true)
-const datasets = ref(await fetch())
-
-const search = ref<string>()
-
-const filteredDatasets = computed(() => {
-  return datasets.value?.filter((d) => d.label.toLowerCase().includes(search.value ?? ''))
-})
-
-async function fetch() {
-  toggleLoading(true)
-  const { data, error } = await DatasetsService.listDatasets().finally(() => toggleLoading(false))
-  return error ? console.error(error) : data
-}
+const headers: CRUDTableHeader[] = [
+  { key: 'label', title: 'Label' },
+  {
+    key: 'sites',
+    title: 'Sites',
+    width: 0,
+    value(item, fallback) {
+      return item.sites.length
+    }
+  }
+]
 </script>
 
 <style scoped></style>
