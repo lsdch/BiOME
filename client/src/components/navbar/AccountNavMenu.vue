58f266ebfa9b5e5053b534421983ef59b08fe654
Louis Duchemin <ls.duchemin@gmail.com>
2024-12-19 14:31:56 +0100

    cosmetic changes to account nav menu

--- client/src/components/navbar/AccountNavMenu.vue
+++ client/src/components/navbar/AccountNavMenu.vue
@@ -1,10 +1,9 @@
 <template>
   <v-btn
     v-if="user === undefined"
-    :variant="smAndDown ? 'flat' : 'outlined'"
-    :prepend-icon="smAndDown ? '' : 'mdi-account-circle'"
+    :variant="smAndDown ? 'flat' : 'tonal'"
+    :prepend-icon="smAndDown ? '' : 'mdi-account'"
     :icon="smAndDown ? 'mdi-account' : undefined"
-    :color="smAndDown ? 'primary' : undefined"
     :text="smAndDown ? '' : 'Sign in'"
     :to="{
       name: 'login',
@@ -14,53 +13,54 @@
           : { redirect: $router.currentRoute.value.path }
     }"
   />
-  <v-menu
-    v-else
-    location="bottom right"
-    :close-on-content-click="false"
-    target="#app-bar"
-    :width="300"
-    :min-width="100"
-  >
-    <template v-slot:activator="{ props }">
-      <v-btn icon="mdi-account-circle" v-bind="props" />
-    </template>
-    <v-list>
-      <v-list-subheader class="mb-3">
-        <div class="d-flex align-center">
-          <v-icon class="mr-5" v-bind="roleIcon(user.role)" />
-          <div>
-            <span class="text-overline">
-              {{ user.identity.full_name }}
-            </span>
-            <br />
-            <span class="text-caption">
-              {{ user.role }}
-            </span>
+  <v-avatar v-else class="mx-2" variant="outlined" color="blue">
+    <v-menu
+      location="bottom right"
+      :close-on-content-click="false"
+      target="#app-bar"
+      :width="300"
+      :min-width="100"
+    >
+      <template v-slot:activator="{ props }">
+        <v-btn icon="mdi-account" variant="flat" color="light-blue-darken-2" v-bind="props" />
+      </template>
+      <v-list>
+        <v-list-subheader class="mb-3">
+          <div class="d-flex align-center">
+            <v-icon class="mr-5" v-bind="roleIcon(user.role)" />
+            <div>
+              <span class="text-overline">
+                {{ user.identity.full_name }}
+              </span>
+              <br />
+              <span class="text-caption">
+                {{ user.role }}
+              </span>
+            </div>
           </div>
-        </div>
-      </v-list-subheader>
-      <v-divider />
-      <v-list-item>
-        <v-btn
-          prepend-icon="mdi-account"
-          variant="plain"
-          density="compact"
-          text="Account"
-          :to="{ name: 'account' }"
-        />
-      </v-list-item>
-      <v-list-item>
-        <v-btn
-          prepend-icon="mdi-power"
-          variant="plain"
-          density="compact"
-          text="Logout"
-          @click="logout()"
-        />
-      </v-list-item>
-    </v-list>
-  </v-menu>
+        </v-list-subheader>
+        <v-divider />
+        <v-list-item>
+          <v-btn
+            prepend-icon="mdi-account"
+            variant="plain"
+            density="compact"
+            text="Account"
+            :to="{ name: 'account' }"
+          />
+        </v-list-item>
+        <v-list-item>
+          <v-btn
+            prepend-icon="mdi-power"
+            variant="plain"
+            density="compact"
+            text="Logout"
+            @click="logout()"
+          />
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-avatar>
 </template>
 
 <script setup lang="ts">
@@ -68,7 +68,7 @@ import { useFeedback } from '@/stores/feedback'
 import { useUserStore } from '@/stores/user'
 import { storeToRefs } from 'pinia'
 import { useRouter } from 'vue-router'
-import { useDisplay } from 'vuetify'
+import { useDisplay, useTheme } from 'vuetify'
 import { roleIcon } from '../people/userRole'
 
 const { smAndDown } = useDisplay()
