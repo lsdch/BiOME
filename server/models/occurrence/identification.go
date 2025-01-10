d619454390fbbcb18fa38e2594282a82f09f2bf4
Louis Duchemin <ls.duchemin@gmail.com>
2025-01-10 11:06:16 +0100

    feat: add IdentificationUpdate struct and update query methods for identification management

--- server/models/occurrence/identification.go
+++ server/models/occurrence/identification.go
@@ -1,9 +1,11 @@
 package occurrence
 
 import (
+	"darco/proto/db"
 	"darco/proto/models"
 	"darco/proto/models/people"
 	"darco/proto/models/taxonomy"
+	"fmt"
 
 	"github.com/edgedb/edgedb-go"
 )
@@ -20,4 +22,38 @@ type IdentificationInput struct {
 	Taxon        string                       `edgedb:"taxon" json:"taxon"`
 	IdentifiedBy models.OptionalInput[string] `edgedb:"identified_by" json:"identified_by"`
 	IdentifiedOn DateWithPrecisionInput       `edgedb:"identified_on" json:"identified_on"`
+}
+
+type IdentificationUpdate struct {
+	Taxon        models.OptionalInput[string]                 `edgedb:"taxon" json:"taxon,omitempty"`
+	IdentifiedBy models.OptionalNull[string]                  `edgedb:"identified_by" json:"identified_by,omitempty"`
+	IdentifiedOn models.OptionalInput[DateWithPrecisionInput] `edgedb:"identified_on" json:"identified_on,omitempty"`
+}
+
+func (u IdentificationUpdate) UpdateQuery(fieldName string) string {
+	return u.FieldMappings(fieldName).Query(u)
+}
+
+func (u IdentificationUpdate) FieldMappings(fieldName string) db.UpdateQuery {
+	return db.UpdateQuery{
+		Frame: fmt.Sprintf(`#edgeql
+			with identification := <json>$0
+			update %s set {
+				%%s # completed using Mappings
+			}
+		`, fieldName),
+		Mappings: map[string]string{
+			"taxon": `#edgeql
+				assert_exists(
+					select taxonomy::Taxon filter .name = <str>data['taxon']
+				)`,
+			"identified_by": `#edgeql
+				assert_exists(
+					select people::Person filter .alias = <str>data['identified_by']
+				)`,
+			"identified_on": `#edgeql
+				date::from_json_with_precision(<json>data['identified_on'])
+			`,
+		},
+	}
 }
