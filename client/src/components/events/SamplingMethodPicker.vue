4494e892bfb09056bf9b10c301cfd275389f7d46
Louis Duchemin <ls.duchemin@gmail.com>
2025-01-26 18:28:27 +0100

    feat: refactor SamplingMethodPicker to use Vue Query for data fetching and error handling

--- client/src/components/events/SamplingMethodPicker.vue
+++ client/src/components/events/SamplingMethodPicker.vue
@@ -1,20 +1,21 @@
 <template>
   <v-autocomplete
     :items
-    :loading
+    :loading="isPending"
     item-title="label"
     item-subtitle="description"
     v-bind="$attrs"
     prepend-inner-icon="mdi-hook"
+    :error-messages="error?.detail"
   >
   </v-autocomplete>
 </template>
 
 <script setup lang="ts">
-import { SamplingService } from '@/api'
-import { useFetchItems } from '@/composables/fetch_items'
+import { listSamplingMethodsOptions } from '@/api/gen/@tanstack/vue-query.gen'
+import { useQuery } from '@tanstack/vue-query'
 
-const { items, loading } = useFetchItems(SamplingService.listSamplingMethods)
+const { data: items, isPending, error } = useQuery(listSamplingMethodsOptions())
 </script>
 
 <style scoped lang="scss"></style>
