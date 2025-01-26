1eedf61bfec5119430801d38776cd957b1d8ff64
Louis Duchemin <ls.duchemin@gmail.com>
2025-01-26 18:21:32 +0100

    feat: enhance HomeView layout with improved footer structure and GitHub link

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,20 +1,37 @@
 <script setup lang="ts">
 import DashBoard from '../components/dashboard/DashBoard.vue'
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
 </script>
 
 <template>
-  <div class="d-flex flex-column justify-space-between fill-height">
+  <v-sheet min-height="100%" class="d-flex flex-column justify-space-between fill-height">
     <DashBoard />
-    <v-footer class="flex-grow-0">
-      <v-row justify="center">
-        <v-btn variant="text"> About </v-btn>
-        <v-btn variant="text" :to="router.resolve({ name: 'api-docs' })"> API Docs </v-btn>
+  </v-sheet>
+  <v-footer border="t-sm">
+    <v-container fluid>
+      <v-row justify="space-between" align="center">
+        <div></div>
+        <div>
+          <v-btn variant="text"> About </v-btn>
+          <v-btn variant="text" :to="{ name: 'api-docs' }"> API Docs </v-btn>
+        </div>
+        <v-btn
+          density="compact"
+          stacked
+          text="BiOME v0.0"
+          variant="text"
+          class="font-monospace text-caption"
+          color=""
+          active-color="primary"
+          target="_blank"
+          href="https://github.com/lsdch/biome"
+        >
+          <template #prepend>
+            <v-icon size="x-large">mdi-github</v-icon>
+          </template>
+        </v-btn>
       </v-row>
-    </v-footer>
-  </div>
+    </v-container>
+  </v-footer>
 </template>
 
 <style scoped lang="less"></style>
