3a0f3dcaf59333b630d1210ddbf91d4d6468aae1
Louis Duchemin <ls.duchemin@gmail.com>
2025-01-27 15:18:24 +0100

    feat: enhance CenteredSpinner component to support custom text and prepend slot

--- client/src/components/toolkit/ui/CenteredSpinner.tsx
+++ client/src/components/toolkit/ui/CenteredSpinner.tsx
@@ -1,12 +1,21 @@
+import { SetupContext, SlotsType, VNodeChild } from 'vue'
 import { ComponentProps } from 'vue-component-type-helpers'
 import { VProgressCircular } from 'vuetify/components'
 
-type CenteredSpinnerProps = ComponentProps<typeof VProgressCircular> & { height?: number | string }
+type CenteredSpinnerProps = ComponentProps<typeof VProgressCircular> & {
+  height?: number | string
+  text?: string
+}
 
-export function CenteredSpinner(props: CenteredSpinnerProps) {
+export function CenteredSpinner(
+  props: CenteredSpinnerProps,
+  context: SetupContext<unknown, SlotsType<{ prepend: () => VNodeChild }>>
+) {
   return (
     <v-sheet class="d-flex align-center justify-center" height={props.height}>
-      <v-progress-circular indeterminate {...props} />
+      {context.slots.prepend?.()}
+      <v-progress-circular indeterminate {...props} class="mx-1" />
+      {props.text}
     </v-sheet>
   )
 }
