d64ed6f1b091dd4b050b467085ef1621c39de3ab
Louis Duchemin <ls.duchemin@gmail.com>
2025-02-19 10:32:21 +0100

    feat: use autocomplete component for datasetpicker

--- client/src/components/datasets/DatasetPicker.vue
+++ client/src/components/datasets/DatasetPicker.vue
@@ -1,32 +1,94 @@
 <template>
-  <v-select
+  <v-autocomplete
     v-model="model"
-    :label
     :items
-    :loading="isPending"
+    :multiple="multiple"
+    :chips="multiple"
+    :closable-chips="multiple"
+    :label="label"
     item-title="label"
-    return-item
+    auto-select-first
+    clear-on-select
+    :loading="loading"
+    :item-value
+    v-bind="$attrs"
     :error-messages="error?.detail"
   >
-    <template #item="{ props, item }">
+    <template v-for="(_, name) in $slots" #[name]="slotData">
+      <slot :name="name" v-bind="slotData" />
+    </template>
+    <template #prepend-item>
+      <div class="d-flex w-100 justify-center">
+        <v-btn-toggle
+          rounded
+          multiple
+          v-model="categories"
+          class="flex-grow-1"
+          color="primary"
+          variant="text"
+          density="compact"
+          divided
+          border="sm"
+        >
+          <v-btn
+            :icon="DatasetCategory.icon('Site')"
+            size="small"
+            value="Site"
+            class="flex-grow-1"
+            title="Sites"
+          />
+          <v-btn
+            :icon="DatasetCategory.icon('Occurrence')"
+            size="small"
+            value="Occurrence"
+            class="flex-grow-1"
+            title="Occurrences"
+          />
+          <v-btn
+            :icon="DatasetCategory.icon('Seq')"
+            size="small"
+            value="Seq"
+            class="flex-grow-1"
+            title="Sequences"
+          />
+        </v-btn-toggle>
+      </div>
+      <v-divider class="mt-2" />
+    </template>
+    <template #item="{ item, props }">
       <v-list-item v-bind="props">
+        <template #prepend="{ isSelected }" v-if="multiple">
+          <v-checkbox :modelValue="isSelected" hide-details density="compact" class="mx-1" />
+        </template>
         <template #append>
-          <v-chip rounded> {{ item.raw.sites.length }}</v-chip>
+          <DatasetCategoryIcon :category="item.raw.category" size="x-small" />
         </template>
       </v-list-item>
     </template>
-  </v-select>
+  </v-autocomplete>
 </template>
 
-<script setup lang="ts">
-import { SiteDataset } from '@/api'
-import { listSiteDatasetsOptions } from '@/api/gen/@tanstack/vue-query.gen'
+<script setup lang="ts" generic="ModelValue extends unknown | unknown[] | null | undefined">
+import { Dataset, DatasetCategory } from '@/api'
+import { listDatasetsOptions } from '@/api/gen/@tanstack/vue-query.gen'
+import DatasetCategoryIcon from '@/components/datasets/DatasetCategoryIcon'
 import { useQuery } from '@tanstack/vue-query'
+import { computed, ref } from 'vue'
 
-const model = defineModel<SiteDataset>()
-withDefaults(defineProps<{ label?: string }>(), { label: 'Dataset' })
+const model = defineModel<ModelValue>()
+const categories = ref<DatasetCategory[]>(['Occurrence', 'Site', 'Seq'])
 
-const { data: items, error, isPending } = useQuery(listSiteDatasetsOptions())
+defineProps<{
+  multiple?: boolean
+  label: string
+  itemValue?: keyof Dataset
+}>()
+
+const { data: datasets, isPending: loading, error } = useQuery(listDatasetsOptions({ query: {} }))
+
+const items = computed(
+  () => datasets.value?.filter(({ category }) => categories.value.includes(category)) ?? []
+)
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
