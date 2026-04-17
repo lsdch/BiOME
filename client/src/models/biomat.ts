819647021cc12e3e9eb3baa48fda11c0c959f4b0
Louis Duchemin <ls.duchemin@gmail.com>
2026-04-17 15:52:04 +0200

    fix: transformation for biomaterial model

--- client/src/models/biomat.ts
+++ client/src/models/biomat.ts
@@ -1,20 +1,20 @@
-import { Article, DateWithPrecisionInput, OccurrenceInput } from "@/api";
-import { IdentificationModel } from "@/components/forms/occurrence/IdentificationFormFields.vue";
-import { reactive, Reactive } from "vue";
+import { Article, DateWithPrecisionInput, OccurrenceInput } from '@/api'
+import { IdentificationModel } from '@/components/forms/occurrence/IdentificationFormFields.vue'
+import { reactive, Reactive } from 'vue'
 
-export type SpecimenQuantityRangeModel = { lower?: number, upper?: number }
+export type SpecimenQuantityRangeModel = { lower?: number; upper?: number }
 export type SpecimenQuantityModel = number | SpecimenQuantityRangeModel
 
-export type BiomatModel = Omit<OccurrenceInput, "published_in" | "identification" | "quantity"> & {
+export type BiomatModel = Omit<OccurrenceInput, 'published_in' | 'identification' | 'quantity'> & {
   identification: IdentificationModel
-  published_in?: Article[],
+  published_in?: Article[]
   quantity?: SpecimenQuantityModel
 }
 
 export function initialModel(): Reactive<BiomatModel> {
   return reactive({
     identification: {
-      identified_on: { precision: 'Day', date: {} },
+      identified_on: { precision: 'Day', date: {} }
     }
   })
 }
@@ -25,11 +25,16 @@ export function toRequestData({ identification, ...model }: BiomatModel): Occurr
     published_in: model.published_in?.map(({ code }) => code),
     identification: {
       taxon: identification.taxon!.name,
-      identified_by: identification.identified_by!.alias,
-      identified_on: identification.identified_on.precision === 'Unknown' ? undefined : identification.identified_on as DateWithPrecisionInput
+      identified_by: identification.identified_by,
+      identified_on:
+        identification.identified_on.precision === 'Unknown'
+          ? undefined
+          : (identification.identified_on as DateWithPrecisionInput)
     },
-    quantity: model.quantity ? (
-      typeof model.quantity === 'number' ? [model.quantity, model.quantity] : [model.quantity.lower!, model.quantity.upper!])
+    quantity: model.quantity
+      ? typeof model.quantity === 'number'
+        ? [model.quantity, model.quantity]
+        : [model.quantity.lower!, model.quantity.upper!]
       : undefined
   }
 }
